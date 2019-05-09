e3ea7bc9244c85c294eec059d514fe123e2527b5
kimseulbi <ksb5166@gmail.com>
2019-05-10 00:32:29 +0900

    api 인증키 발급 후 추가

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 export const weatherURL = "https://api.openweathermap.org/data/2.5/weather";
 export const weatherKEY = "ad3d7608a1b5adb0d472c9c74b4d71cb";
 export const dustURL =
-  "http://openapi.seoul.go.kr:8088/49485542476c636836386164734f63/json/ListAvgOfSeoulAirQualityService/1/5/";
+  "http://openapi.seoul.go.kr:8088/744b5567696b736232357a5653507a/json/ListAvgOfSeoulAirQualityService/1/5/";
