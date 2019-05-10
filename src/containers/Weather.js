3bdbf6dc1baf40065fa622d71b62d4aeca8dcf59
kimseulbi <ksb5166@gmail.com>
2019-05-11 00:24:13 +0900

    Loading상태 화면 추가

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -3,13 +3,7 @@ import { connect } from "react-redux";
 import { fetchWeather } from "../store/ducks/weather";
 import { fetchDust } from "../store/ducks/dust";
 
-import {
-  getTemp,
-  getIcon,
-  getCity,
-  getDate,
-  getWeather
-} from "../modules/index";
+import { getTemp, getIcon, getCity, getWeather } from "../modules/index";
 
 import WeatherView from "../components/WeatherView";
 import DustView from "../components/DustView";
@@ -18,7 +12,7 @@ class Weather extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: getDate()
+      date: new Date()
     };
   }
 
@@ -28,25 +22,38 @@ class Weather extends Component {
   }
 
   render() {
-    const { city, temp, weather, icon } = this.props;
-    const { pm10, pm25, ozone, nitrogen } = this.props;
-    return (
-      <>
-        <WeatherView
-          city={getCity(city)}
-          icon={getIcon(icon)}
-          temp={getTemp(temp)}
-          weather={getWeather(weather)}
-        />
-        <DustView
-          pm10={pm10}
-          pm25={pm25}
-          ozone={ozone}
-          nitrogen={nitrogen}
-          date={this.state.date}
-        />
-      </>
-    );
+    const {
+      city,
+      temp,
+      weather,
+      icon,
+      loading,
+      pm10,
+      pm25,
+      ozone,
+      nitrogen
+    } = this.props;
+    if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          <WeatherView
+            city={getCity(city)}
+            icon={getIcon(icon)}
+            temp={getTemp(temp)}
+            weather={getWeather(weather)}
+          />
+          <DustView
+            pm10={pm10}
+            pm25={pm25}
+            ozone={ozone}
+            nitrogen={nitrogen}
+            date={this.state.date}
+          />
+        </>
+      );
+    }
   }
 }
 
