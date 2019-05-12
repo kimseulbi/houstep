d5a38bd62da37ee9e2371ebf0d8476bd93766e1a
kimseulbi <ksb5166@gmail.com>
2019-05-13 00:19:47 +0900

    react-app-polyfill 패키지 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "react-app-polyfill/ie10";
 import "./scss/index.scss";
 import Root from "./components/Root";
 import { configureStore } from "./configureStore";
